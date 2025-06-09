5c22f582ab7a16c51b391c106557efe392e4ea43
Arnaldo Tayao Jr <arnaldotayaojr@gmail.com>
2025-06-09 17:44:00 +0800

    Adds which-key grouping

--- lua/siyomai/plugins/which-key.lua
+++ lua/siyomai/plugins/which-key.lua
@@ -1,22 +1,12 @@
-local status, which_key = pcall(require, 'which-key')
-if not status then
-  return
-end
+local status, wk = pcall(require, 'which-key')
+if not status then return end
 
-which_key.setup({
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
-})
+wk.setup({})
+
+wk.add({
+  { "<leader>g", group = "+Git" },                 -- group
+  { "<leader>l", group = "+Session" },             -- group
+  { "<leader>s", group = "+Search" },              -- group
+  { "<leader>t", group = "+Tabs" },                -- group
+  { "<leader>w", group = "+Workspaces" },          -- group
+}, { mode = "n", ignore_missing = true })

4d38cd285fd7df19b761dde695b34d5bdca9983c
Arnaldo Tayao Jr <arnaldotayaojr@gmail.com>
2025-06-10 10:55:58 +0800

    Adds copilot dependency into plugins-setup.lua file

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["CopilotChat.nvim"] = {
+    loaded = true,
+    path = "/Users/arnaldotayaojr/.local/share/nvim/site/pack/packer/start/CopilotChat.nvim",
+    url = "https://github.com/CopilotC-Nvim/CopilotChat.nvim"
+  },
   LuaSnip = {
     loaded = true,
     path = "/Users/arnaldotayaojr/.local/share/nvim/site/pack/packer/start/LuaSnip",
@@ -83,6 +88,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/arnaldotayaojr/.local/share/nvim/site/pack/packer/start/auto-session",
     url = "https://github.com/rmagatti/auto-session"
+  },
+  ["avante.nvim"] = {
+    config = { "\27LJ\2\n8\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\vavante\frequire\0" },
+    loaded = true,
+    path = "/Users/arnaldotayaojr/.local/share/nvim/site/pack/packer/start/avante.nvim",
+    url = "https://github.com/yetone/avante.nvim"
   },
   ["cmp-buffer"] = {
     loaded = true,
@@ -108,6 +119,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/arnaldotayaojr/.local/share/nvim/site/pack/packer/start/comment.nvim",
     url = "https://github.com/numToStr/comment.nvim"
+  },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/Users/arnaldotayaojr/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "git@github.com:github/copilot.vim"
   },
   ["formatter.nvim"] = {
     loaded = true,
@@ -248,6 +264,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: avante.nvim
+time([[Config for avante.nvim]], true)
+try_loadstring("\27LJ\2\n8\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\vavante\frequire\0", "config", "avante.nvim")
+time([[Config for avante.nvim]], false)
 -- Config for: org-bullets.nvim
 time([[Config for org-bullets.nvim]], true)
 try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\16org-bullets\frequire\0", "config", "org-bullets.nvim")
